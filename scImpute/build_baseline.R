af1f19ab7a690a82cd4c7838e1180b702ea2e803
rrchai <73901500+rrchai@users.noreply.github.com>
2022-11-09 15:52:12 -0800

    add macs2 and update codes

--- scImpute/build_baseline.R
+++ scImpute/build_baseline.R
@@ -12,34 +12,27 @@ args <- commandArgs(trailingOnly = TRUE)
 input_dir <- args[1]
 out_dir <- args[2]
 
-# read the filenames of all downsampled data
-# basenames <- read.table(file.path(args[1], "scrna_input_basenames.txt"), header = FALSE)[, 1]
-# basenames <- c("ds3r_c0_p20k_n1","ds3r_c0_p20k_n2","ds3r_c0_p20k_n3",
-#                "ds3r_c0_p10k_n1","ds3r_c0_p10k_n2","ds3r_c0_p10k_n3")
-# input_filenames <- paste0(basenames, ".csv")
-# output_filenames <- paste0(basenames, "_imputed.csv")
-#
-# t0 <- Sys.time()
-#
-# for (i in seq_along(input_filenames)) {
-#
-#   cat(">>>>>>>>>>Imputing ", input_filenames[i], "\n")
-#   scimpute(file.path(input_dir, input_filenames[i]),
-#            # ensure to have a '/' at last, otherwise scImpute will fail since it uses paste0
-#            out_dir = file.path(out_dir, ""),
-#            file_name = output_filenames[i],
-#            Kcluster = 3,
-#            ncores = ncores)
-# }
+# Get input files
+input_filenames <- list.files(input_dir, pattern = "*.csv")
+# Retrieve the filenames without ext of all downsampled data
+filenames <- tools::file_path_sans_ext(input_filenames)
+# Add "_imputed" to create prediction/output filenames
+output_filenames <- paste0(filenames, "_imputed.csv")
 
-# compress all predictions into a tarball
-# utils::tar(
-#   tarfile = file.path(out_dir "/predictions.tar.gz"),
-#   files = file.path(out_dir, output_data)
-# )
-# or use CLI
-# system(sprintf("tar -I pigz -cvf %s/predictions.tar.gz %s/*_imputed.csv", out_dir, out_dir))
+t0 <- Sys.time()
 
-# print(Sys.time() - t0)
+for (i in seq_along(input_filenames)) {
+    cat(">>>>>>>>>>Imputing ", input_filenames[i], "\n")
+    scimpute(file.path(input_dir, input_filenames[i]),
+        # ensure to have a '/' at last, otherwise scImpute will fail since it uses paste0
+        out_dir = file.path(out_dir, ""),
+        file_name = output_filenames[i],
+        Kcluster = 3,
+        ncores = ncores
+    )
+}
 
-system("cp /input/scimpute.tar.gz /output/predictions.tar.gz")
+system(sprintf("tar -I pigz -cvf %s/predictions.tar.gz %s/*_imputed.csv", out_dir, out_dir))
+
+# Quick getting results from scimpute - for admin purpose
+# system("cp /input/scimpute.tar.gz /output/predictions.tar.gz")
