2221a17a9188d3f5f3a6e868f37306972dc7ccb2
rrchai <73901500+rrchai@users.noreply.github.com>
2024-04-09 18:06:02 -0400

    not remove origin bed file

--- macs2/run_model.sh
+++ macs2/run_model.sh
@@ -8,5 +8,6 @@ mkdir -p $OUPUT_DIR
 basename -a -s .bam $(ls $INPUT_DIR/*.bam) | parallel --max-procs=$NCORES --halt-on-error 2 \
   'macs2 callpeak --keep-dup all -t $INPUT_DIR/{}.bam --nomodel --shift 100 --extsize 200 -n {} --outdir $OUPUT_DIR &&
    perl /01.filter_peaks.pl $OUPUT_DIR/{}_peaks.narrowPeak $OUPUT_DIR/{} 500 &&
-   mv $OUPUT_DIR/{}.500.bed $OUPUT_DIR/{}.bed &&
-   rm -f $OUPUT_DIR/{}_summits.bed'
+   mv $OUPUT_DIR/{}.500.bed $OUPUT_DIR/{}.bed
+   '
+# && rm -f $OUPUT_DIR/{}_summits.bed
